225842ca552657f351ea0bd43349c6480281a82f
sharonre77 <sharon@mirage-web.com>
2026-04-28 19:22:06 +0300

    add zone

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.micro"
 }
+variable "instance_zone" {
+    description = "zone of the instance"
+    default = "eu-north-1"
+}
+
